25a4b49a3adebfe79224af5691506b47849e8693
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-02 15:36:09 -0700

    Created an info route

---
+++ lib/infoRoute.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class InfoRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'text'
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'This is the body'
+        ),
+      ),
+    );
+  }
+}
