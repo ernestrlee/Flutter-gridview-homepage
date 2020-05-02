424a0a223730eb0c8972e7696a10adcecf1433b2
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-01 18:16:29 -0700

    Added gem data. Corrected file names.

---
+++ lib/gridData.dart
@@ -0,0 +1,60 @@
+enum Gems {
+  amethyst,
+  apatite,
+  aquamarine,
+  citrine,
+  diamond,
+  emerald,
+  ruby,
+  sapphire,
+  spinel,
+}
+
+var gridData = [
+  {
+    'name': 'Diamond',
+    'imageFileName': 'diamond.png',
+    'value': 'diamond',
+  },
+  {
+    'name': 'Ruby',
+    'imageFileName': 'ruby.png',
+    'value': 'ruby',
+  },
+  {
+    'name': 'Amethyst',
+    'imageFileName': 'amethyst.png',
+    'value': 'amethyst',
+  },
+  {
+    'name': 'Citrine',
+    'imageFileName': 'citrine.png',
+    'value': 'citrine',
+  },
+  {
+    'name': 'Emerald',
+    'imageFileName': 'emerald.png',
+    'value': 'emerald',
+  },
+  {
+    'name': 'Sapphire',
+    'imageFileName': 'sapphire.png',
+    'value': 'sapphire',
+  },
+  {
+    'name': 'Spinel',
+    'imageFileName': 'spinel.png',
+    'value': 'spinel',
+  },
+  {
+    'name': 'Apatite',
+    'imageFileName': 'apatite.png',
+    'value': 'apatite',
+  },
+  {
+    'name': 'Aquamarine',
+    'imageFileName': 'aquamarine.png',
+    'value': 'aquamarine',
+  },
+
+];
