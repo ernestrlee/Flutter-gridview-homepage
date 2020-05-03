68b70ce327c23b155410454f792d2afc52aae109
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-03 15:37:42 -0700

    Added settings list

--- lib/settingsRoute.dart
+++ lib/settingsRoute.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class SettingsRoute extends StatelessWidget {
   final String appBarTitle;
+  final List<String> settingsList = [
+    'BackgroundColor',
+    'AppBarColor',
+  ];
 
   SettingsRoute({this.appBarTitle});
 
@@ -10,20 +14,25 @@ class SettingsRoute extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-            appBarTitle,
+          appBarTitle,
         ),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'text'
+      body: ListView.separated(
+        padding: EdgeInsets.all(8),
+        itemCount: settingsList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                  child: Text(settingsList[index]),
+              ),
             ),
-            Text(
-              'text2'
-            ),
-          ],
-        ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => Divider(),
       ),
     );
   }
